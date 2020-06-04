2fb05f66352dde1b6d7a96bc03d0fdf30ad5fede
Micheal Hansen <micheal.hansen@gmail.com>
2020-06-04 10:50:27 -0700

    add javascript to make text boxes appear and disappear without clicking on another animal
    Co-authored-by: Tristan Emmerson <tristan@stickerslug.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,19 +1,45 @@
 $(document).ready(function() {
   $("div#snakeHead").click(function() {
-    $("div#turtle").hide();
-    $("div#spider").hide();
-    $("div#snake").fadeIn();
+    if (marker("div#snake")) {
+      $("div#snake").fadeOut();
+      $("div#snake").removeClass("shown")
+    } else {
+      $("div#turtle").hide();
+      $("div#spider").hide();
+      $("div#snake").fadeIn();
+      $("div#snake").addClass("shown")
+    };
   });
 
   $("div#spiderHead").click(function() {
-    $("div#turtle").hide();
-    $("div#snake").hide();
-    $("div#spider").fadeIn();
+    if (marker("div#spider")) {
+      $("div#spider").fadeOut();
+      $("div#spider").removeClass("shown");
+    } else {
+      $("div#turtle").hide();
+      $("div#snake").hide();
+      $("div#spider").fadeIn();
+      $("div#spider").addClass("shown");
+    };
   });
 
   $("div#turtleHead").click(function() {
-    $("div#spider").hide();
-    $("div#snake").hide();
-    $("div#turtle").fadeIn();
+    if (marker("div#turtle")) {
+      $("div#turtle").fadeOut();
+      $("div#turtle").removeClass("shown");
+    } else {
+      $("div#spider").hide();
+      $("div#snake").hide();
+      $("div#turtle").fadeIn();
+      $("div#turtle").addClass("shown");
+    };
   });
 })
+
+function marker(clsName) {
+  if ($(clsName).attr('class').slice(-5) === 'shown') {
+    return true;
+  } else {
+    return false;
+  };
+}
